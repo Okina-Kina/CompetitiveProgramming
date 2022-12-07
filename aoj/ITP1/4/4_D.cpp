ff732e7ac10555a7fafbc932cc1719d63ba435c4
root <root@MyComputer.localdomain>
2022-12-07 23:01:36 +0900

    solve ITP1_3

---
+++ aoj/ITP1/4/4_D.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    cout << ans << endl;
+
+    return 0;
+}
